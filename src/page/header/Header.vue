9094040d40853be02ae250ba0702cea38f48b4d9
fumihiko-kariya27 <fumihiko_kariya@icloud.com>
2025-11-17 22:46:16 +0900

    ヘッダー画面の追加

---
+++ src/page/header/Header.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useAuthStore } from '@/stores/authStore';
+const authStore = useAuthStore();
+
+const logout = () => {
+    authStore.logout();
+}
+</script>
+<template>
+  <div class="flex items-center justify-between w-full">
+    <div class="text-3xl md:text-4xl font-bold text-gray-800">
+      トレーニング管理システム
+    </div>
+    <div v-show="authStore.isAuthenticated">
+        <el-dropdown trigger="click">
+            <div class="font-bold flex items-center space-x-2 cursor-pointer">
+                <el-avatar class="bg-yellow-500">
+                {{ authStore.user?.charAt(0).toUpperCase() }}
+                </el-avatar>
+                <span>{{ authStore.user }}</span>
+            </div>
+        </el-dropdown>
+        <el-button @click="logout()">ログアウト</el-button>
+    </div>
+  </div>
+</template>
