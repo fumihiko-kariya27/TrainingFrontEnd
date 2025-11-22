70263204dc35f5bb502edcea48c8bde453e064b2
fumihiko-kariya27 <fumihiko_kariya@icloud.com>
2025-11-22 09:55:23 +0900

    ユーザー権限による画面表示切り替え処理の追加

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -2,12 +2,15 @@
 import HomeItem from './HomeItem.vue';
 import { User, Notebook } from '@element-plus/icons-vue';
 import { useTrainingStore } from '@/stores/trainingStore';
+import { useAuthStore } from '@/stores/authStore';
 
 const trainingStore = useTrainingStore();
 
 async function fetchTrainings(){
   await trainingStore.getTrainings();
 }
+
+const authStore = useAuthStore();
 </script>
 <template>
   <div class="bg-white shadow-lg rounded-lg p-6 md:p-8">
@@ -19,7 +22,7 @@ async function fetchTrainings(){
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <!-- ユーザー一覧画面 -->
-      <HomeItem title="ユーザー一覧" link="/users" :icon="User">
+      <HomeItem v-if="authStore.isAdmin()" title="ユーザー一覧" link="/users" :icon="User">
         登録されているユーザー情報を確認・管理します。
       </HomeItem>
       <!-- トレーニング一覧画面 -->
